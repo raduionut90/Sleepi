75050d1cd666b13cf3048f78c595c106dbcc49e6
radu <raduionutgeorgian@gmail.com>
2022-04-24 20:28:16 +0300

    Initial Commit

---
+++ Sleepi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sleepi
+//
+//  Created by Ionut Radu on 24.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sleepi/SleepiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepiApp.swift
+//  Sleepi
+//
+//  Created by Ionut Radu on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SleepiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
